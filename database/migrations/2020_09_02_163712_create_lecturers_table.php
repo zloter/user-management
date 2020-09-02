77f4b7e66e7ed47143c03e24280a8f3190a73cd9
Zygmunt Loter <zygmuntloter@gmail.com>
2020-09-02 16:55:08 +0200

    Database structure

---
+++ database/migrations/2020_09_02_163712_create_lecturers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('phone', 20);
+            $table->tinyInteger('education');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturers');
+    }
+}
---
+++ database/migrations/2020_09_02_164243_create_employees_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('address_voivodeship', 100);
+            $table->string('address_city', 100);
+            $table->string('address_postal_code', 10);
+            $table->string('address_street', 100);
+            $table->string('address_number', 10);
+            $table->string('correspondence_voivodeship', 100);
+            $table->string('correspondence_city', 100);
+            $table->string('correspondence_postal_code', 10);
+            $table->string('correspondence_street', 100);
+            $table->string('correspondence_number', 10);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
